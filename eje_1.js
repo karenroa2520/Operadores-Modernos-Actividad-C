a636df06777813963a0aa382878b73e9bc32e22b
michelle_roa <roakaren2507@gmail.com>
2025-12-01 16:30:54 -0500

    base ejercicio 1

---
+++ eje_1.js
@@ -0,0 +1,39 @@
+const promt = require("prompt-sync")();
+
+
+function procesarPedido(pedido, ...extras) {
+    // Destructuración del objeto pedido
+    const { cliente, producto, cantidad } = pedido;
+    
+    // Crear un objeto con la información consolidada
+    const pedidoConsolidado = {
+        cliente,
+        producto,
+        cantidad,
+        extras: [...extras] // Almacenar los extras en un array
+    };
+    
+    return pedidoConsolidado;
+}
+
+// Ejemplo de uso
+const pedido = {
+    cliente: "Juan Pérez",
+    producto: "Laptop",
+    cantidad: 2
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ practica.js
@@ -0,0 +1,83 @@
+// const promt = require("prompt-sync")();
+// const nombre = promt("Ingrese su nombre: ");
+
+// console.log(nombre);
+
+
+// const promtt = require("prompt-sync")();
+
+// while (true) {
+//   const numero = parseInt(promt("Ingrese el numero: "));
+//   if (!numero % 2 == 0) {
+//     console.log("Numero invalido");
+//     continue
+//   }
+//   break;
+// }
+// console.log("Final");
+
+// const promt = require("prompt-sync")();
+// const arreglo = []
+// while (true) {
+//   let numero = parseInt(promt("Ingrese el numero, si desea dejar de agregar ingrese el vaolor de 0: "));
+//   if (numero >= 0){
+//     arreglo.push(numero);
+//     continue;
+//   }
+//   break;
+// }
+
+// function promedio(arreglo){
+//   console.log(arreglo);
+// }
+
+// let arreglo = [1, 2, 3, 4];
+// let arrelgoA = [5, 6, 7, 8]
+// let arregloB = arregloA.concat(arreglo);
+
+// console.log(arreglo);
+// console.log(arrelgoA);
+// console.log(arregloB);
+// arregloC.shift()
+// console.log(arregloB);
+// console.log(arregloC);
+// arreglo.shift();
+// arreglo.shift();
+// arreglo.shift();
+// console.log
+
+
+// const persona = {};
+
+// persona.nombre = "michelle";
+// persona.apellido = "mendoza";
+
+// console.log(persona);
+
+// const aprendiz = {...persona}
+
+// aprendiz.nombre = "darcy";
+
+
+// try {
+//   let a = "dos";
+//   let b = 25;
+//   if (isNaN(a) || isNaN(b)) {
+//     throw new Error ("Ingrese valores permitidos");
+//   }
+
+//   let c = a + b;
+//   console.log(c);
+
+// } catch (error) {
+//   console.log(error.message);
+  
+// }
+
+
+
+
+
+
+
+
